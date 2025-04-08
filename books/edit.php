e5ed10d3aa603df9c25181aafdb856e45103435d
hastel <mugendieric13@gmail.com>
2025-04-08 10:26:33 +0300

    ..

--- books/edit.php
+++ books/edit.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../config/config.php'; 
+require_once '../includes/config.php'; 
 require_once '../includes/auth_functions.php';
 require_once '../includes/book_functions.php';
 
